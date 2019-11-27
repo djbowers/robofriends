3cb276511f6f35a2089f8e5d35e4319be8615165
DJ Bowers <dj@dj-bowers.com>
2019-11-27 12:02:23 -0800

    Updates

---
+++ precache-manifest.811d6aad1858a9382951ec555fb14012.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fa01903d3b3ea5f4639f260e445a11b",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "26dafc4245b14a37f593",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "5a52aafaf287655be6a7",
+    "url": "/robofriends/static/css/main.4417a45f.chunk.css"
+  },
+  {
+    "revision": "26dafc4245b14a37f593",
+    "url": "/robofriends/static/js/2.1f39b60e.chunk.js"
+  },
+  {
+    "revision": "5a52aafaf287655be6a7",
+    "url": "/robofriends/static/js/main.29a24778.chunk.js"
+  },
+  {
+    "revision": "7b4f849e958ac4107787",
+    "url": "/robofriends/static/js/runtime-main.a1071882.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/robofriends/static/media/SEGA.82e09f78.TTF"
+  }
+]);
